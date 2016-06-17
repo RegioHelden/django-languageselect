a7a90b93a4c581527e5732083ad9d97744b9cced
Mounir <messelmeni.mounir@gmail.com>
2016-06-17 16:26:45 +0200

    Fixing urls warning in Django 1.9

--- django-languageselect/urls.py
+++ django-languageselect/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-    url(r"^/?", views.IndexView.as_view(), name="languageselect_index"),
+    url(r"^\?", views.IndexView.as_view(), name="languageselect_index"),
 )
