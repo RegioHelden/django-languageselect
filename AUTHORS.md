afdfb49d0128837337f0df6e77ce62dbc2935368
Jens Nistler <jens.nistler@regiohelden.de>
2024-06-27 15:27:30 +0200

    change docs to markdown

---
+++ AUTHORS.md
@@ -0,0 +1,6 @@
+# Credits
+
+## Contributors
+
+* [Jens Nistler](https://github.com/lociii)
+* [Mounir Messelmeni](https://github.com/mounirmesselmeni)
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_version(*file_paths):
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.md') as history_file:
     history = history_file.read()
 
 requirements = [
